68007031e0fbda5e1f736f326d41bef4d877a29d
Deblina Mukherjee <40189262+deblnia@users.noreply.github.com>
2024-04-26 02:37:55 +0000

    finished ch 4

--- exercises/04_primitive_types/primitive_types4.rs
+++ exercises/04_primitive_types/primitive_types4.rs
@@ -5,13 +5,14 @@
 // Execute `rustlings hint primitive_types4` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 #[test]
 fn slice_out_of_array() {
     let a = [1, 2, 3, 4, 5];
 
-    let nice_slice = ???
+    let nice_slice = &a[1..4];
+    // so rust indices are inclusive
+    // from the book: 
+        // where starting_index is the first position in the slice and ending_index is one more than the last position in the slice. 
 
     assert_eq!([2, 3, 4], nice_slice)
 }
